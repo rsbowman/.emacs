18bcb310e286b7acf1f5fc670dd63b437af960a5
Sean Bowman <r.sean.bowman@gmail.com>
2011-10-03 18:00:46 -0500

    autoload python mode, dont pause on redisplay

--- init-python-mode.el
+++ init-python-mode.el
@@ -1,5 +1,6 @@
 (sean-add-package 'python)
 
+(autoload 'python-mode "python-mode" "Python Mode." t)
 (add-to-list 'auto-mode-alist '("\\.py$" . python-mode))
 (add-to-list 'interpreter-mode-alist '("python" . python-mode))
 (autoload 'python-mode "python-mode" "Python editing mode." t)
--- init.el
+++ init.el
@@ -24,6 +24,8 @@
 (setq-default indent-tabs-mode nil)
 (setq inhibit-startup-message t)
 
+(setq redisplay-dont-pause t)
+
 (fset 'yes-or-no-p 'y-or-n-p)
 
 (delete-selection-mode t)
