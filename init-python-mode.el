d5299e3c47ed39359c98c39b9eeb82d457e8b43f
Sean Bowman <r.sean.bowman@gmail.com>
2012-02-28 18:00:57 -0600

    dont autoload python

--- init-python-mode.el
+++ init-python-mode.el
@@ -1,9 +1,9 @@
 (sean-add-package 'python)
+(require 'python)
 
-(autoload 'python-mode "python-mode" "Python Mode." t)
 (add-to-list 'auto-mode-alist '("\\.py$" . python-mode))
 (add-to-list 'interpreter-mode-alist '("python" . python-mode))
-(autoload 'python-mode "python-mode" "Python editing mode." t)
+;(autoload 'python-mode "python-mode" "Python editing mode." t)
 
 (add-hook 'python-mode-hook 
           (lambda () (define-key python-mode-map "\C-m" 'newline-and-indent)))
