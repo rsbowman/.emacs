fe35227f4c81051e2c3bca29f7db7b5b533b143a
Sean Bowman <r.sean.bowman@gmail.com>
2012-02-28 18:08:55 -0600

    add org mode and compile

---
+++ init-org.el
@@ -0,0 +1,19 @@
+;(sean-add-package 'org-mode)
+
+(require 'org-install)
+(add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
+
+; org latex export
+(require 'org-latex)
+(unless (boundp 'org-export-latex-classes)
+  (setq org-export-latex-classes nil))
+(add-to-list 'org-export-latex-classes
+             '("article"
+               "\\documentclass{article}"
+               ("\\section{%s}" . "\\section*{%s}")
+               ("\\subsection{%s}" . "\\subsection*{%s}")
+               ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
+               ("\\paragraph{%s}" . "\\paragraph*{%s}")
+               ("\\subparagraph{%s}" . "\\subparagraph*{%s}")))
+
+(provide 'init-org)
