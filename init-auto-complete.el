be88d3279d630437c4217e6b3b373a6a122045a8
Sean Bowman <r.sean.bowman@gmail.com>
2012-12-24 21:51:36 -0600

    get rid of autocomplete because I have no clue how it works

--- init-auto-complete.el
+++ init-auto-complete.el
@@ -1,71 +1,79 @@
 (sean-add-package 'auto-complete)
 
-(require 'auto-complete)
-(global-auto-complete-mode t)
-(setq ac-auto-start nil)
-(setq ac-dwim nil) ; To get pop-ups with docs even if a word is uniquely completed
-(define-key ac-completing-map (kbd "C-n") 'ac-next)
-(define-key ac-completing-map (kbd "C-p") 'ac-previous)
+;; (require 'auto-complete)
+;; (require 'auto-complete-config)
+;; (require 'auto-complete-clang)
 
-;;----------------------------------------------------------------------------
-;; Use Emacs' built-in TAB completion hooks to trigger AC (Emacs >= 23.2)
-;;----------------------------------------------------------------------------
-(setq tab-always-indent 'complete)  ;; use 'complete when auto-complete is disabled
-(add-to-list 'completion-styles 'initials t)
+;; (defun my-ac-cc-mode-setup ()
+;;   (setq ac-sources (append '(ac-source-clang ac-source-yasnippet) ac-sources)))
+;; (add-hook 'c-mode-common-hook 'my-ac-cc-mode-setup)
 
-;; hook AC into completion-at-point
-(defun set-auto-complete-as-completion-at-point-function ()
-  (setq completion-at-point-functions '(auto-complete)))
-(add-hook 'auto-complete-mode-hook 'set-auto-complete-as-completion-at-point-function)
+;; (require 'auto-complete)
+;; (global-auto-complete-mode t)
+;; (setq ac-auto-start nil)
+;; (setq ac-dwim nil) ; To get pop-ups with docs even if a word is uniquely completed
+;; (define-key ac-completing-map (kbd "C-n") 'ac-next)
+;; (define-key ac-completing-map (kbd "C-p") 'ac-previous)
 
-;;;
-; use yasnippet for autocomplete
-(defun ac-yasnippet-candidate ()
-  (let ((table (yas/get-snippet-tables major-mode)))
-    (if table
-      (let (candidates (list))
-            (mapcar (lambda (mode)          
-              (maphash (lambda (key value)    
-                (push key candidates))          
-              (yas/snippet-table-hash mode))) 
-            table)
-        (all-completions ac-prefix candidates)))))
+;; ;;----------------------------------------------------------------------------
+;; ;; Use Emacs' built-in TAB completion hooks to trigger AC (Emacs >= 23.2)
+;; ;;----------------------------------------------------------------------------
+;; (setq tab-always-indent 'complete)  ;; use 'complete when auto-complete is disabled
+;; (add-to-list 'completion-styles 'initials t)
 
-(defface ac-yasnippet-candidate-face
-  '((t (:background "sandybrown" :foreground "black")))
-  "Face for yasnippet candidate.")
+;; ;; hook AC into completion-at-point
+;; (defun set-auto-complete-as-completion-at-point-function ()
+;;   (setq completion-at-point-functions '(auto-complete)))
+;; (add-hook 'auto-complete-mode-hook 'set-auto-complete-as-completion-at-point-function)
 
-(defface ac-yasnippet-selection-face
-  '((t (:background "coral3" :foreground "white"))) 
-  "Face for the yasnippet selected candidate.")
+;; ;;;
+;; ; use yasnippet for autocomplete
+;; (defun ac-yasnippet-candidate ()
+;;   (let ((table (yas/get-snippet-tables major-mode)))
+;;     (if table
+;;       (let (candidates (list))
+;;             (mapcar (lambda (mode)          
+;;               (maphash (lambda (key value)    
+;;                 (push key candidates))          
+;;               (yas/snippet-table-hash mode))) 
+;;             table)
+;;         (all-completions ac-prefix candidates)))))
 
-(defvar ac-source-yasnippet
-  '((candidates . ac-yasnippet-candidate)
-    (action . yas/expand)
-    (limit . 3)
-    (candidate-face . ac-yasnippet-candidate-face)
-    (selection-face . ac-yasnippet-selection-face)) 
-  "Source for Yasnippet.")
+;; (defface ac-yasnippet-candidate-face
+;;   '((t (:background "sandybrown" :foreground "black")))
+;;   "Face for yasnippet candidate.")
 
-;;;
+;; (defface ac-yasnippet-selection-face
+;;   '((t (:background "coral3" :foreground "white"))) 
+;;   "Face for the yasnippet selected candidate.")
 
-(set-default 'ac-sources
-             '(ac-source-yasnippet
-               ac-source-dictionary
-               ac-source-words-in-buffer
-               ac-source-words-in-same-mode-buffers
-               ac-source-words-in-all-buffer))
+;; (defvar ac-source-yasnippet
+;;   '((candidates . ac-yasnippet-candidate)
+;;     (action . yas/expand)
+;;     (limit . 3)
+;;     (candidate-face . ac-yasnippet-candidate-face)
+;;     (selection-face . ac-yasnippet-selection-face)) 
+;;   "Source for Yasnippet.")
 
-(dolist (mode '(magit-log-edit-mode log-edit-mode org-mode text-mode haml-mode
-                                    sass-mode yaml-mode csv-mode espresso-mode haskell-mode
-                                    html-mode nxml-mode sh-mode smarty-mode clojure-mode
-                                    lisp-mode textile-mode markdown-mode tuareg-mode LaTeX-mode))
-  (add-to-list 'ac-modes mode))
+;; ;;;
 
-;; Exclude very large buffers from dabbrev
-(defun smp-dabbrev-friend-buffer (other-buffer)
-  (< (buffer-size other-buffer) (* 1 1024 1024)))
+;; (set-default 'ac-sources
+;;              '(ac-source-yasnippet
+;;                ac-source-dictionary
+;;                ac-source-words-in-buffer
+;;                ac-source-words-in-same-mode-buffers
+;;                ac-source-words-in-all-buffer))
 
-(setq dabbrev-friend-buffer-function 'smp-dabbrev-friend-buffer)
+;; (dolist (mode '(magit-log-edit-mode log-edit-mode org-mode text-mode haml-mode
+;;                                     sass-mode yaml-mode csv-mode espresso-mode haskell-mode
+;;                                     html-mode nxml-mode sh-mode smarty-mode clojure-mode
+;;                                     lisp-mode textile-mode markdown-mode tuareg-mode LaTeX-mode))
+;;   (add-to-list 'ac-modes mode))
+
+;; ;; Exclude very large buffers from dabbrev
+;; (defun smp-dabbrev-friend-buffer (other-buffer)
+;;   (< (buffer-size other-buffer) (* 1 1024 1024)))
+
+;; (setq dabbrev-friend-buffer-function 'smp-dabbrev-friend-buffer)
 
 (provide 'init-auto-complete)
