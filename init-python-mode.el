f642763e80002d76fc17b2bd6bc55847dc723097
Sean Bowman <r.sean.bowman@gmail.com>
2014-01-22 15:06:44 -0600

    indent 4 spaces in python mode, pair $ in latex mode

--- init-python-mode.el
+++ init-python-mode.el
@@ -6,6 +6,7 @@
 ;(autoload 'python-mode "python-mode" "Python editing mode." t)
 
 (add-hook 'python-mode-hook 
-          (lambda () (define-key python-mode-map "\C-m" 'newline-and-indent)))
+          (lambda () (define-key python-mode-map "\C-m" 'newline-and-indent)
+            (set-variable 'py-indent-offset 4)))
 
 (provide 'init-python-mode)
